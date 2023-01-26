9a90c51977a23cdcd62582c85fb5ca22e111a4b2
yuki <shimazuyuki7@gmail.com>
2023-01-26 10:56:35 +0900

    fix: env

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,7 +8,7 @@ const Home = () => {
   return (
     <Layout>
       <div className="page">
-        <h1>Hello world - fixed</h1>
+        <h1>Hello world - fixed env</h1>
         <main></main>
       </div>
       <style jsx>{`
